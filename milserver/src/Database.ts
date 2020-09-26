78887a6f5f4f3bf48f0408b9d835302c9e99f13b
sd <sunil.desai@bhge.com>
2020-09-26 22:11:07 +0530

    ts changes

--- milserver/src/Database.ts
+++ milserver/src/Database.ts
@@ -1,31 +1,32 @@
+
 class Database {
-public MilitaryDatabase = {
+  public MilitaryDatabase = {
     "users":
-        [
-            {
-                userid: "MX12345678",
-                password: "mil123"
-            }
-        ],
+      [
+        {
+          userid: "MX12345678",
+          password: "mil123"
+        }
+      ],
 
     "insession": [
 
     ],
 
     "places": [
-        "Lachulung La",
-        "Sia La",
-        "Tanglang La",
-        "Khardung La",
-        "Sasser Pass",
-        "Zoji La",
-        "Rezang La",
-        "Marsimik La",
-        "Gyong La",
-        "Indira Col",
-        "Pensi La"
+      "Lachulung La",
+      "Sia La",
+      "Tanglang La",
+      "Khardung La",
+      "Sasser Pass",
+      "Zoji La",
+      "Rezang La",
+      "Marsimik La",
+      "Gyong La",
+      "Indira Col",
+      "Pensi La"
     ]
-}
+  }
 }
 
 export let database = new Database();
