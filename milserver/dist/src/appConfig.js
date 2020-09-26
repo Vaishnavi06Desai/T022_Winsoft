008f5a2c1b244a3d29729d12fd0d00ca156978dc
sd <sunil.desai@bhge.com>
2020-09-26 22:40:56 +0530

    cleanup

--- milserver/dist/src/appConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configuration = void 0;
-exports.configuration = {
-    'webport': 5001
-};
-//# sourceMappingURL=appConfig.js.map
