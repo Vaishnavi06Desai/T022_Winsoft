4d90ae3dc53d6e349d8068523bc6aa950cc79238
sd <sunil.desai@bhge.com>
2020-09-27 13:18:03 +0530

    added userinput component

--- djhack/src/app/decrypt/decrypt.component.ts
+++ djhack/src/app/decrypt/decrypt.component.ts
@@ -17,49 +17,51 @@ export class DecryptComponent implements OnInit {
   constructor(private httpClient: HttpClient, private router: Router, private auth: AuthService, private app: AppComponent) { }
 
 
-  decoded: "";
-  infiltrated: "";
+  decoded: '';
+  infiltrated: '';
+
+
+  url = 'http://localhost:5001/message';
+
+  decstring = {
+    'message': 'Cnwvtus KuaiTaa rlodeeurethn  an Ia_mrhs baer oag ndC_a aeoat dLj lLdio_me  p  hagZLngan _',
+    'key': 'delhi'
+  };
+
+
+  form = new FormGroup({
+    message: new FormControl(''),
+    key: new FormControl('')
+  });
+
   ngOnInit(): void {
-    if(localStorage.getItem('token') == "null")
-    {
+    if (localStorage.getItem('token') === 'null') {
       this.router.navigate(['/login']);
     }
     this.auth.setValuel(false);
   }
 
-  url = "http://localhost:5001/message";
 
-  decstring = {
-    "message": "Cnwvtus KuaiTaa rlodeeurethn  an Ia_mrhs baer oag ndC_a aeoat dLj lLdio_me  p  hagZLngan _",
-    "key": "delhi"
-}
-
-form = new FormGroup({
-  message: new FormControl(''),
-  key: new FormControl('')
- })
-
-
-  decryptstring(decstring){
+  decryptstring(decstring) {
     let header_node = {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem('token'))
-      };
-      // let input = new FormData();
-      // input.append("file", file);
-      // input.append("docType", data);
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
+    };
+    // let input = new FormData();
+    // input.append("file", file);
+    // input.append("docType", data);
     this.httpClient.post<any>(this.url, decstring, header_node).subscribe(
-      (res) => {console.log(res); this.decoded = res.message; this.infiltrated = res.infiltrated; this.app.messageapp = res.message; this.app.placesapp = res.infiltrated ;this.router.navigate(['/userinput'])},
+      (res) => { console.log(res); this.decoded = res.message; this.infiltrated = res.infiltrated; this.app.messageapp = res.message; this.app.placesapp = res.infiltrated; this.router.navigate(['/userinput']); },
       (err) => {
-                  console.log(err); 
-                  // if(err.status == 0 || err.status == 500)
-                  // {this.setValuee(true)}
-                  // else if(err.status == 401)
-                  // {this.setValueu(true)}
-              }
+        console.log(err);
+        // if(err.status == 0 || err.status == 500)
+        // {this.setValuee(true)}
+        // else if(err.status == 401)
+        // {this.setValueu(true)}
+      }
     );
   }
 
-  submit(){
+  submit() {
     this.decryptstring(this.form.value);
   }
 }
--- djhack/src/app/userinput/userinput.component.ts
+++ djhack/src/app/userinput/userinput.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-userinput',
@@ -8,9 +9,46 @@ import { AppComponent } from '../app.component';
 })
 export class UserinputComponent implements OnInit {
 
+  // formGroup = new FormGroup();
+  form = new FormGroup({
+    message: new FormControl(''),
+    key: new FormControl('')
+  });
+
+  baseList: any = [
+    { id: 1, name: 'Tanglang La' },
+    { id: 2, name: 'Khardung La' },
+    { id: 3, name: 'Sasser Pass' },
+    { id: 4, name: 'Zoji Lam' },
+    { id: 5, name: 'Rezang La' },
+    { id: 6, name: 'Marsimik La' },
+    { id: 7, name: 'Gyong La' },
+    { id: 8, name: 'Indira Col' },
+    { id: 9, name: 'Eemsi La' }
+
+  ];
+
   constructor(private app: AppComponent) { }
 
   ngOnInit(): void {
   }
 
+  submit(): void {
+
+  }
+
+  onCheckboxChange(e) {
+    // const website: FormArray = this.form.get('website') as FormArray;
+
+    // if (e.target.checked) {
+    //   website.push(new FormControl(e.target.value));
+    // } else {
+    //    const index = website.controls.findIndex(x => x.value === e.target.value);
+    //    website.removeAt(index);
+    // }
+  }
+
+  log(x) {
+    console.log('>>>>', x);
+  }
 }
