41f9e60359111f2c19db6ed045daf58c383d4ace
unknown <angelsindisguise999@gmail.com>
2020-09-27 11:20:25 +0530

    commit 8

--- milserver/src/graph.ts
+++ milserver/src/graph.ts
@@ -57,6 +57,61 @@ class Dijkstra {
     // return [hopstot, min, finnode];
     return {"us": finnode, "them": targets}
   }
+
+  public findnode2(targets: string[], exBases: string[]): IBases {
+    console.log(targets)
+    let hopstot: IHopsTot = {};
+    for (let node of nodes) {
+      if (targets.includes(node))
+        continue;
+      else {
+        let hops = 0;
+        let visited = []
+        visited.push(node)
+        let temp: {[key: string]: number}
+        temp = {}
+        temp[node] = 0;
+        let queue: {[key: string]: number}
+        queue = {}
+        queue[node] = 0;
+        var queueit: string[] = []
+        var infil = targets.slice()
+        var curr_node:string = node;
+        queueit.push(node)
+        while(queueit != [])
+        {
+          hops = queue[curr_node]
+          // curr_node = queueit.pop()
+          // for(var c_node of graph[curr_node])
+          // {
+          //   queue[c_node] = hops + 1;
+          // }
+        }
+        // for (let target of targets) {
+        //   var source = node;
+        //   var dest = target;
+        //   hops += ((route.path(source, dest)).length - 1)
+        // }
+        // // dict = {}
+        // // dict[node] = hops;
+        // hopstot[node] = hops
+      }
+    }
+    let min = 9999;
+    let finnode = '';
+    for (let node in hopstot) {
+      if (hopstot[node] < min) {
+        min = hopstot[node];
+        finnode = node;
+      }
+    }
+    console.log(route.path("A", "L"));
+
+    // return [hopstot, min, finnode];
+    return {"us": finnode, "them": targets}
+  }
 }
+
+
 
 export let dijkstra = new Dijkstra();
